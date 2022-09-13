040350e81495468277c3b9e3a29fff5a638c7f05
Gregory Curran <gregoryc@dpr.com>
2022-09-12 20:32:30 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+your_name = input("What should I call you")
+print("wowza",your_name, "is a really dope name.")
+your_major = input("What are you majoring in?")
+print("Well",your_name,"Comp151 will really help with",your_major)
+message1 = 'I Said "I Want single quotes here'""
+message2 = "I can't what use double quotes"
+comment_to_user = f"wow{your_name} is a really cool name and a cool{your_major}"
+print(comment_to_user)
